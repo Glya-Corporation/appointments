ae79791e741bb15645378244fdc1b919427c103a
alfonsouzcategui2@gmail.com <alfonsouzcategui2@gmail.com>
2023-10-19 01:01:13 -0500

    update modal

--- frontend/src/components/ModalAddServises.jsx
+++ frontend/src/components/ModalAddServises.jsx
@@ -1,17 +1,43 @@
 import {Modal, Button} from 'react-bootstrap'
+import {useForm} from 'react-hook-form'
 
 
 const ModalAddServices = ({data, ...props}) => {
+  
+  const {handlesubmit, register, reset} = useForm();
+  
+  const submit = data => {
+    console.log(data)
+  }
+  
   return (
     <Modal {...props}>
       <Modal.Header>
         <Modal.Title>{data.title}</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <form>
-          <input type='text' placeholder='Name'/>
-          <input type='text' placeholder='price'/>
-          <input type='date'/>
+        <form onSubmit={handlesubmit(submit)}>
+          <div>
+          <input type='text' placeholder='Nonmbre' {...register('name1')}/>
+          <input type='text' placeholder='Price' {...register('price1')}/>
+          <input type='date' placeholder='Duración' {...register('duration1')}/>
+          </div>
+          <div>
+          <input type='text' placeholder='Nonmbre' {...register('name2')}/>
+          <input type='text' placeholder='Price' {...register('price2')}/>
+          <input type='date' placeholder='Duración' {...register('duration2')}/>
+          </div>
+          <div>
+          <input type='text' placeholder='Nonmbre' {...register('name3')}/>
+          <input type='text' placeholder='Price' {...register('price3')}/>
+          <input type='date' placeholder='Duración' {...register('duration3')}/>
+          </div>
+          <div>
+          <input type='text' placeholder='Nonmbre' {...register('name4')}/>
+          <input type='text' placeholder='Price' {...register('price4')}/>
+          <input type='date' placeholder='Duración' {...register('duration4')}/>
+          </div>
+          <button>Crear</button>
         </form>
       </Modal.Body>
       <Modal.Footer>
