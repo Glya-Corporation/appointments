fb44aa7f5236450ee29a51a59f4c22bff8a72a59
alfonsouzcategui2@gmail.com <alfonsouzcategui2@gmail.com>
2023-10-22 10:25:39 -0500

    styles height

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Settings = () => {
   return (
-    <div>
-      
+    <div className='settings-main'>
+      <h3>Configuraciones</h3>
+      <ul>
+        <li>Temas</li>
+        <li>Días laborables</li>
+        <li>Colores</li>
+        <li>Pagos</li>
+      </ul>
     </div>
   );
 }
